517334df0ae2b95df23d126f0a53874028a336b1
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-03-31 22:22:51 -0400

    Updating api get

--- utils/api.ts
+++ utils/api.ts
@@ -1,10 +1,22 @@
 import axios from 'axios';
+import { ApiResponse } from '@customTypes/apiTypes';
+import { normalizeKeyCasing } from '@utils';
 
-export const get = async (url: string, options?: Record<string, any>) => {
+const get = async (
+  url: string,
+  options?: Record<string, any>,
+): Promise<ApiResponse> => {
   try {
     const response = await axios.get(url, options);
-    return response.data;
+    return {
+      data: normalizeKeyCasing(response.data) || {},
+    };
   } catch (error) {
     console.error(error);
+    return {
+      data: null,
+    };
   }
 };
+
+export default { get };
