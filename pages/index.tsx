e6519db33d90c8569e911ec7fe445ae766f8134c
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-03-31 21:31:12 -0400

    Showing get response in HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
-const HomePage = () => <h1>Hello from HomePage!</h1>;
+import { useEffect, useState } from 'react';
+import { get } from 'utils/api';
+import styles from '@styles/HomePage.module.scss';
+
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    get('https://api.openbrewerydb.org/breweries').then((res) => {
+      setData(res);
+      setIsLoading(false);
+    });
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className={styles.HomePage}>
+      <h1>Data loaded!</h1>
+      <code>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </code>
+    </div>
+  );
+};
 
 export default HomePage;
