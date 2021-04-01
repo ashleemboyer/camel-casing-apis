8fa9f122ae756b61fc030c8740304c8176d480ba
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-03-31 21:08:43 -0400

    Adding initial pages and styles

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app';
+import '@styles/global.scss';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+const HomePage = () => <h1>Hello from HomePage!</h1>;
+
+export default HomePage;
